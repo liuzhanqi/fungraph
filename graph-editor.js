fd135534e959497eba92b16889112b84e02ec53b
LZQ <liuzhanqi0909@gmail.com>
2015-05-10 16:20:35 +0800

    add edge

--- graph-editor.js
+++ graph-editor.js
@@ -24,43 +24,79 @@ document.onload = (function (d3, saveAs, Blob, undefined) {
 	    }
 	    var id = "Node_"+Node.count;
 	    this.x=x;this.y=y;this.id=id;
+	    drawNode(this);
+	    toggleSelectNode(this);
 
-  		var updateColor = function (node) {
+  		function updateColor (node) {
+  			if (node == null) return;
 			if (graph.state.selectedNode != node) {
-	    		d3.select(node).style("fill", "white");
+	    		d3.select("#"+node.id).style("fill", "white");
 	    	} else {
-	    		d3.select(node).style("fill", "LightCoral");
+	    		d3.select("#"+node.id).style("fill", "LightCoral");
 	    	}
 		};
 
-  		var newNode = svg.append("circle")
-	        .style("stroke", "gray")
-	        .style("fill", "white")
-	        .attr("r", 40)
-	        .attr("cx", x)
-	        .attr("cy", y)
-	        .attr("id", id)
-	        .call(dragNode)
-	        .on("mouseover", function(){
-	        	d3.select(this).style("fill", "aliceblue");
-	        })
-	        .on("mouseout", function(){
-	        	updateColor(this);
-	        })
-	        .on("click", function() {
-	        	//click on node to select or deselect
-	        	if (graph.state.selectedNode == this) {
-	        		graph.state.selectedNode = null;
-	        		d3.select(this).style("fill", "white");
-	        	} else {
-	        		oldNode = graph.state.selectedNode;
-	        		graph.state.selectedNode = this;
-	        		if (oldNode != null) {
-	        			updateColor(oldNode);
-	        		}
-	        		updateColor(this);
-	        	}
-	        });
+		function drawNode(node) { 
+  			svg.append("circle")
+		        .style("stroke", "gray")
+		        .style("fill", "white")
+		        .attr("r", 40)
+		        .attr("cx", node.x)
+		        .attr("cy", node.y)
+		        .attr("id", node.id)
+		        .call(dragNode)
+		        .on("mouseover", function(){
+		        	if (graph.state.selectedNode!=node )
+		        		d3.select("#"+node.id).style("fill", "aliceblue");
+		        })
+		        .on("mouseout", function(){
+		        	updateColor(node);
+		        })
+		        .on("click", function() {
+		        	oldnode = graph.state.selectedNode;
+		        	if (oldnode!=null && oldnode!=node && d3.event.shiftKey) {
+		        		var newEdge = new Edge(svg,node,oldnode);
+		        	}
+		        	toggleSelectNode(node);
+		        });
+  		};
+
+  		function toggleSelectNode(node) {
+  			//cancel selection
+  			if (graph.state.selectedNode == node) {
+        		graph.state.selectedNode = null;
+        		d3.select("#"+node.id).style("fill", "white");
+        	} else {
+        		oldnode = graph.state.selectedNode;
+        		graph.state.selectedNode = node;
+        		updateColor(oldnode);
+        		updateColor(node);
+        	}
+  		};
+
+  	}
+
+  	var Edge = function(svg, node1, node2) {
+  		if (Edge.count == undefined) {
+	    	Edge.count = 1;
+	    } else {
+	    	Edge.count ++;
+	    }
+  		var x1=node1.x, y1=node1.y;
+  		var x2=node2.x, y2=node2.y;
+	    var id = "Edge_"+Edge.count;
+	    this.node1=node1;this.node2=node2;
+	    this.x1=x1;this.y1=y1;
+	    this.x2=x2;this.y2=y2;
+	    this.id=id;
+	    svg.append("line")
+			.style("stroke", "gray")
+			.style("stroke-width", "3")
+			.attr("x1", x1)
+			.attr("y1", y1)
+			.attr("x2", x2)
+			.attr("y2", y2)
+			.attr("id", id);
   	}
 
 	var GraphCreator = function(svg, nodes, edges) {
@@ -95,15 +131,15 @@ document.onload = (function (d3, saveAs, Blob, undefined) {
 	    d3.select(window)
 	    .on("keydown", function(){
 	    	//thisGraph.svgKeyDown.call(thisGraph);
-	    	console.log("key down");
+	    	//console.log("key down");
 	    })
 	    .on("keyup", function(){
 	    	//thisGraph.svgKeyUp.call(thisGraph);
-	    	console.log("key up");
+	    	//console.log("key up");
 	    })
 	    svg.on("mousedown", function(d){
-	    	//thisGraph.svgMouseDown.call(thisGraph, d);
-	    	//console.log("mouse down");
+	    	if (d3.event.shiftKey) {
+	    	}
 	    });
 	    svg.on("mouseup", function(d){
 	    	//thisGraph.svgMouseUp.call(thisGraph, d);
@@ -113,12 +149,8 @@ document.onload = (function (d3, saveAs, Blob, undefined) {
 	    	console.log("double click at "+d3.mouse(this)+", created a node");
 	    	var newNode = new Node(svg,d3.mouse(this)[0],d3.mouse(this)[1]);
 	    	thisGraph.nodes.push(newNode);
-	    	// for (var i in thisGraph.nodes) {
-		   	// 	console.log(thisGraph.nodes[i].id);
-	    	// }
 	    });
 	}
-
 
 	var docEL = document.documentElement,
 		bodyEL = document.getElementsByTagName('body')[0];
